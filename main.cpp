38e07abfedde48fa605c19f12357440391d3da3e
Ali-Hon <ali.ottawa.canada@gmail.com>
2024-12-18 22:30:35 -0500

    Add forward declarations; fix checkAttempt if clause

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
-#include <cctype>
+#include <string>
 #include <algorithm>
 
 #include "constants.h"
 
 using namespace constants;
+
+
+void displayBoard(std::string_view board[][NUM_OF_ATTEMPTS]);
+std::string getInput();
+void parseInput(std::string_view input);
+int* checkAttempt(std::string_view attempt, std::string_view word);
+
+int main(){
+    std::string word {"arise"};
+
+    std::cout << "\n";
+    std::string_view input = getInput();
+    parseInput(input);
+    int* map = checkAttempt(input, word);
+    for(int i = 0; i<WORD_LENGTH; i++){
+        std::cout << map[i] << "\n";
+    }
+
+    return 0;
+}
 
 void displayBoard(std::string_view board[][NUM_OF_ATTEMPTS]){
     for(int i = 0; i < WORD_LENGTH; i++){
@@ -44,7 +64,7 @@ int* checkAttempt(std::string_view attempt, std::string_view word){
     static int map[WORD_LENGTH] {}; // zero-initiliazation
     for(int i = 0; i < WORD_LENGTH; i++){
         char c = attempt[i];
-        if(word.find(c) != std::string::npos)
+        if(word.find(c) == std::string::npos)
             map[i] = 0;
         else if(word.find(c) != attempt.find(c) && word.find(c) != std::string::npos)
             map[i] = 1;
@@ -52,14 +72,4 @@ int* checkAttempt(std::string_view attempt, std::string_view word){
             map[i] = 2;
     }
     return map;
-}
-
-int main(){
-    std::string_view word {"Arise"};
-
-    std::string_view input = getInput();
-    parseInput(input);
-    int* map = checkAttempt(input, word);
-
-    return 0;
 }
