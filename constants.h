08ab5cd7303e0a74c8074b5a04cfeaa18fdbab28
AliCodesJava <ali.ottawa.canada@gmail.com>
2024-12-25 15:08:20 -0500

    Add getRandomWord, refactor ascii a to z constants, add possible answers and guessable words lists

--- constants.h
+++ constants.h
@@ -3,5 +3,7 @@ namespace constants{
     #define CONSTANTS
     const int WORD_LENGTH = 5;
     const int NUM_OF_ATTEMPTS = 6;
+    const int LOWER_CASE_A_ASCII = 97;
+    const int LOWER_CASE_Z_ASCII = 123;
     #endif
 }
--- main.cpp
+++ main.cpp
@@ -4,17 +4,24 @@
 #include <string>
 #include <string_view>
 #include <cstring>
+#include <cstdlib>
+#include <array>
 
 #include "constants.h"
+#include "words.h"
+#include "answers.h"
 
 
 namespace C = constants;
+namespace W = words;
+namespace A = answers;
 
 enum class ATTEMPT_OUTCOMES;
-std::string getInput(std::string_view prompt);
-void parseInput(std::string_view input);
-int* checkAttempt(std::string_view attempt, std::string_view word);
-void displayWordValidity(std::string_view attempt, int map[]);
+std::string_view getRandomWord();
+std::string getInput(std::string_view);
+void parseInput(std::string_view);
+int* checkAttempt(std::string_view, std::string_view);
+void displayWordValidity(std::string_view, int[]);
 
 enum class ATTEMPT_OUTCOMES{
     NOT_IN_WORD = 0,
@@ -22,11 +29,20 @@ enum class ATTEMPT_OUTCOMES{
     IN_WORD_RIGHT_SPOT = 2
 };
 
+std::string_view getRandomWord(){
+    srand(time(0)); // give new seed (here it is the current time)
+    
+    int numOfElements = sizeof(W::WORDS_S) / sizeof(*W::WORDS_S);
+    int r_int = std::rand() % numOfElements;
+
+    return W::WORDS_S[r_int];
+}
+
 int main(){
     std::cout << "Welcome to a Wordle clone!\n";
 
-    const std::string WORD { "arise" };
-    
+    const std::string WORD { getRandomWord() };
+
     int current = 1;
     while(current < C::NUM_OF_ATTEMPTS){
         std::string_view promptPhrase { "\nEnter a word: " };
@@ -64,8 +80,8 @@ void parseInput(std::string_view input){
         return;
     }
 
-    int lo { 97 };
-    int hi { 123 };
+    const int lo = C::LOWER_CASE_A_ASCII;
+    const int hi = C::LOWER_CASE_Z_ASCII;
     for(char c: input){
         int charToInt = std::tolower(c);
         // std::clamp returns first arg if it is within bounds
